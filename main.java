ceef6aa57fbb42f90e10e484bab08d1cbd486490
Agneta Prise <118614802+aprise11@users.noreply.github.com>
2024-04-03 19:19:33 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+
+  var temp = -15; // Example temperature
+
+    if (temp <= 5) {
+        System.out.println("Wear super warm clothing!");
+    } else if (temp <= 15) {
+        System.out.println("It's warm!");
+    } else if (temp <= 30) {
+        System.out.println("It's normal.");
+    } else {
+        System.out.println("You need cooling.");
+    }
+
+  }
+}
